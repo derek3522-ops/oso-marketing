e1140c7dcfc1b79645567654502104e056747cf7
derek3522-ops <derek3522@gmail.com>
2026-07-29 13:57:00 -0700

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -35,7 +35,7 @@ export default function Contact() {
         style={{
           backgroundImage: 'url(/BearRoar.png)',
           backgroundSize: 'cover',
-          backgroundPosition: '60% 40%',
+          backgroundPosition: 'right bottom',
         }}
       >
         {/* Dark overlay for text readability */}
